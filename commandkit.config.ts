8c82618d698e8e8c720443a048e56112bc6d101c
twlite <46562212+twlite@users.noreply.github.com>
2025-05-30 21:04:26 +0545

    feat: devtools plugin

--- commandkit.config.ts
+++ commandkit.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'commandkit/config';
 import { cache } from '@commandkit/cache';
 import { i18n } from '@commandkit/i18n';
+import { devtools } from '@commandkit/devtools';
 
 export default defineConfig({
-  plugins: [cache(), i18n()],
+  plugins: [cache(), i18n(), devtools()],
 });
